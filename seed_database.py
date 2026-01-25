0a8cc50b34aa3e1f8f9dd43abe42f0c44f027a1c
linyi <jianglinyi97@gmail.com>
2026-01-25 13:30:44 -0800

    database seed

--- seed_database.py
+++ seed_database.py
@@ -1,5 +1,6 @@
 import sqlite3
-import sqlite_vss
+import numpy as np
+import sqlite_vec
 import pandas as pd
 from sentence_transformers import SentenceTransformer
 
@@ -11,51 +12,58 @@ model = SentenceTransformer(
 
 db = sqlite3.connect("my_db.db")
 
-# Enable loading extensions and load vss
+# Enable loading extensions and load vec0
 db.enable_load_extension(True)
-sqlite_vss.load(db)
+sqlite_vec.load(db)
+db.enable_load_extension(False)
 
 # Verify installation
-version = db.execute("select vss_version()").fetchone()[0]
-print(f"sqlite-vss version: {version}")
+vec_version, = db.execute("select vec_version()").fetchone()
+print(f"vec_version={vec_version}")
 
 cursor = db.cursor()
 
-# Create a standard table for metadata (text, titles, etc.)
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS documents (
-        row_id INTEGER PRIMARY KEY,
-        business_name TEXT,
-        business_domain TEXT,
-        business_niche_description TEXT
-    )
-''')
+# # Create a standard table for metadata (text, titles, etc.)
+# cursor.execute('''
+#     CREATE TABLE IF NOT EXISTS documents (
+#         row_id INTEGER PRIMARY KEY,
+#         business_name TEXT,
+#         business_domain TEXT,
+#         business_niche_description TEXT
+#     )
+# ''')
 
 cursor.execute("""
-    CREATE VIRTUAL TABLE vss_documents USING vss0(business_name_embedding(384));
+    CREATE VIRTUAL TABLE IF NOT EXISTS vec_documents USING 
+               vec0(
+                business_name_embedding float[4096],
+                +business_name text, 
+                +business_domain text, 
+                +business_niche_description text
+               );
 """)
 
 businesses = pd.read_csv('quebec_business_full_export.csv')
 embeddings = model.encode(businesses['business_name'].tolist(), prompt_name="document")
 
-
 for i, row in businesses.iterrows():
-    name = row['business_name']
-    vector = embeddings[i].tobytes() # Convert numpy array to BLOB
-    
+    vector =   embeddings[i].astype(np.float32)
     # Insert text into documents
-    cursor.execute('''INSERT INTO documents (
-                   row_id, 
+    cursor.execute('''INSERT INTO vec_documents (
+                   rowid, 
+                   business_name_embedding,
                    business_name, 
                    business_domain, 
-                   business_niche_description) VALUES (?, ?, ?, ?)'''
-                   , (i, name, row['business_domain'], row['business_niche_description']))
-    row_id = cursor.lastrowid
-    
-    # Insert vector into index using the SAME row_id
-    cursor.execute("""
-        INSERT INTO vss_documents(row_id, business_name_embedding) VALUES (?, ?); 
-    """, (row_id, vector))
+                   business_niche_description) VALUES (?, ?, ?, ?, ?)'''
+                   , (i, vector, row['business_name'], row['business_domain'], row['business_niche_description']))
     
 db.commit()
+print(cursor.execute("""
+select
+    rowid,
+    business_name,
+    business_name_embedding
+from vec_documents
+limit 2;
+""").fetchall())
 db.close()
