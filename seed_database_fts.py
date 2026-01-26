498c0e497ee312c1907af9d2b98aaf4da373ca15
linyi <jianglinyi97@gmail.com>
2026-01-25 20:01:44 -0800

    test fts

---
+++ seed_database_fts.py
@@ -0,0 +1,30 @@
+import sqlite3
+import pandas as pd
+
+db = sqlite3.connect("my_db.db")
+
+
+db.execute("""
+    CREATE VIRTUAL TABLE IF NOT EXISTS fts_documents USING 
+               fts5(
+                business_name,
+                business_domain, 
+                business_niche_description,
+                tokenize = 'trigram'
+               );
+""")
+
+businesses = pd.read_csv('quebec_business_full_export.csv')
+data = [];
+for i, row in businesses.iterrows():
+    # Insert text into documents
+    data.append((row['business_name'], row['business_domain'], row['business_niche_description']));
+
+db.executemany('''INSERT INTO fts_documents (
+                   business_name, 
+                   business_domain, 
+                   business_niche_description) VALUES (?, ?, ?)'''
+                   , data);    
+db.commit()
+db.close()
+
---
+++ test_fts.py
@@ -0,0 +1,15 @@
+
+import sqlite3
+
+db = sqlite3.connect("my_db.db")
+search_term = 'business_name: DÉELISPHARM INC'
+query = """
+    SELECT business_name,
+            business_domain, 
+            business_niche_description
+    FROM fts_documents 
+    WHERE fts_documents MATCH ? 
+    ORDER BY bm25(fts_documents);
+"""
+
+print(db.execute(query, (search_term,)).fetchall())
